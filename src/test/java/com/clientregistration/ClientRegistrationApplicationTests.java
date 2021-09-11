03d53634a4e16ea91be67138d313cbd1820fa77f
brunoeufrasio <bruno10tere@gmail.com>
2021-09-11 10:57:33 -0300

    commit inicial

---
+++ src/test/java/com/clientregistration/ClientRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
