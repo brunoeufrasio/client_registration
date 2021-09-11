710caa2aa47b704ec991f1be9068c5d359e17fa3
brunoeufrasio <bruno10tere@gmail.com>
2021-09-11 11:20:04 -0300

    classe cliente

---
+++ src/main/java/com/clientregistration/entities/Client.java
@@ -0,0 +1,67 @@
+package com.clientregistration.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.cglib.beans.BeanCopier.Generator;
+
+@Entity
+@Table(name = "client")
+public class Client {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Integer age;
+	private String sex;
+
+	public Client() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Client(Long id, String name, Integer age, String sex) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.sex = sex;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+}
