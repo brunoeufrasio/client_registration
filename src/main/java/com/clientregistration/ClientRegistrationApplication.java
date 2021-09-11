29af63a3d744af756f1e93ce902cb7171eb27f59
brunoeufrasio <bruno10tere@gmail.com>
2021-09-11 17:34:04 -0300

    classe cidade

--- src/main/java/com/clientregistration/ClientRegistrationApplication.java
+++ src/main/java/com/clientregistration/ClientRegistrationApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClientRegistrationApplication {
 
+	/*
+	 * uns 2 cadastros  e vc precisa fazer uma autenticação 
+	 * pra proteger algumas rotas  e deixar outras publicas 
+	 * ai se vc fizer testes, subir com docker ganha pontos kkk
+	 * 
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ClientRegistrationApplication.class, args);
 	}
---
+++ src/main/java/com/clientregistration/entities/City.java
@@ -0,0 +1,54 @@
+package com.clientregistration.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "city")
+public class City {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String state;
+
+	public City() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public City(Long id, String name, String state) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.state = state;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+}
